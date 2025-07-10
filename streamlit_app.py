c7de8026f6060569e3a36da705d3c6f99f1f2902
Pcov79 <covinip@gmail.com>
2025-07-10 14:56:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from io import BytesIO
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
 
-st.title("📊 Backlog Weekly Check Tool")
+st.title("💻 Backlog Weekly Check Tool📊")
 
 # Upload files
 prev_file = st.file_uploader("Upload Previous Week's Backlog", type=["xlsx"])
